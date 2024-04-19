900010e9f9a095c875d8aec494c2376a0b1d86eb
soham6540 <savitafarkade6540@gmail.com>
2024-04-19 14:30:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/eternalsspace")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/eternalsspace")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/nexusworldgc")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/nexusworldgc")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
